4fc2114140bfa563a905b7dff2a1d0a49bad0f94
cesine <cesine@yahoo.com>
2014-12-30 17:21:40 +0700

    using precalculated birthday timestamp

--- scripts/set_ci_version.sh
+++ scripts/set_ci_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-BIRTHDAY="Tue Apr 20 10:00:00 2012";
-BIRTHDAY_TIMESTAMP=$(echo `date -j -f "%a %b %d %T %Y" "$BIRTHDAY" "+%s"`);
+BIRTHDAY="Tue Apr 20 12:00:00 EDT 2012";
+BIRTHDAY_TIMESTAMP=1334941200;
 
 today="$(echo `date`)"
 todayTimestamp="$(echo `date  +%s`)"
